0144df16521a5974e12caf47ce4640be205fec78
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-12-10 15:57:18 +0600

    contact management all complete

--- src/app/(dashboard)/contact-management/page.tsx
+++ src/app/(dashboard)/contact-management/page.tsx
@@ -1,10 +1,12 @@
 import DashboardHeader from '@/components/ui/dashboard-header'
 import React from 'react'
+import ContactManagementContainer from './_components/contact-management-container'
 
 const ContactManagementPage = () => {
   return (
     <div>
         <DashboardHeader title="Contact Management" desc="Ready to compete in your next match?"/>
+        <ContactManagementContainer/>
     </div>
   )
 }
